caca047fa19e5d08bb6d34a8b0a34447d0a7fead
Joe Clapis <jclapis@outlook.com>
2024-05-05 09:45:22 -0400

    Added InvalidArgCountError

--- utils/input/validation.go
+++ utils/input/validation.go
@@ -21,14 +21,34 @@ const (
 	MinPasswordLength int = 12
 )
 
-//
-// General types
-//
+// ==============
+// === Errors ===
+// ==============
+
+type InvalidArgCountError struct {
+	ExpectedCount int
+	ActualCount   int
+}
+
+func (e *InvalidArgCountError) Error() string {
+	return fmt.Sprintf("Incorrect argument count - expected %d but have %d", e.ExpectedCount, e.ActualCount)
+}
+
+func NewInvalidArgCountError(expectedCount int, actualCount int) *InvalidArgCountError {
+	return &InvalidArgCountError{
+		ExpectedCount: expectedCount,
+		ActualCount:   actualCount,
+	}
+}
+
+// ==================
+// === Validation ===
+// ==================
 
 // Validate command argument count
-func ValidateArgCount(argCount int, expectedCount int) error {
+func ValidateArgCount(argCount int, expectedCount int) *InvalidArgCountError {
 	if argCount != expectedCount {
-		return fmt.Errorf("Incorrect argument count; expected %d but have %d", expectedCount, argCount)
+		return NewInvalidArgCountError(expectedCount, argCount)
 	}
 	return nil
 }
